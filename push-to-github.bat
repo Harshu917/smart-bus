251a7e7a4a8c7f579fc8ca7e2ec01c4e95d2601c
Harshu917 <harshu917@users.noreply.github.com>
2026-08-26 19:56:43 +0530

    chore: add 1-click push script

---
+++ push-to-github.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ===================================================
+echo   Pushing Smart Bus Repository to GitHub
+echo ===================================================
+git push -u origin main
+echo ===================================================
+pause
